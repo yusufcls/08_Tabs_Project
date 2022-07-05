f9d51a5bbd997c54ac8d1a633f0a996152d5ab36
Yusuf C <yusufcfly@gmail.com>
2022-07-05 14:07:11 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const API_URL = "https://course-api.com/react-tabs-project";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
