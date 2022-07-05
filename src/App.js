7eb51174802a381efec2985c4dd5ea6e6c33c951
Yusuf C <yusufcfly@gmail.com>
2022-07-05 14:26:17 +0200

    phase_01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
-
+import { useState, useEffect } from "react";
 const API_URL = "https://course-api.com/react-tabs-project";
-
 const App = () => {
-  return <div>App</div>;
+const [apiData, setApiData] = useState();
+const [isLoading, setIsLoading] = useState(true);
+const [value, setValue] = useState(0);
+const fetchData = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      // console.log(data);
+      setApiData(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  if (isLoading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+  console.log(apiData);
+  const { company, dates, duties, id, title } = apiData[value];
+  // console.log(company);
+  return <section className="section">
+    
+  </section>;
 };
-
 export default App;
